913a2c63277a270ce7b1f2c413c658b2d04077cf
fikrifyanto <fikrifyanto@gmail.com>
2025-06-20 19:04:33 +0700

    Add TrustProxies middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->use([
-            TrustProxies::class
-        ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
